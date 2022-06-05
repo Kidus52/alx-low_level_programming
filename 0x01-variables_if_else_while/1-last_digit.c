3b6695b0c0005de8bbe581fecc13ad11f53d3365
kidus52 <kidustigistu3410@gmail.com>
2022-06-05 08:55:48 -0700

    My first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,10 +7,11 @@
 * and whether it is greater than 5, less than 6, or 0.
 *
 * Return: Always 0.
-  */
+*/
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
