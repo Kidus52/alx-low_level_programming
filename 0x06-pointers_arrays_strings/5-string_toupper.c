93bf8f40363ee83345b4704e387eac050ea2deec
kidus52 <kidustigistu3410@gmail.com>
2022-06-15 10:51:52 -0700

    my commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,28 +2,20 @@
 #include <stdio.h>
 
 /**
- * cap_string - change to starting letter to uppercase
+ * string_toupper - change to uppercase
  * @s: string
  * Return: uppercase string
  */
 
-char *cap_string(char *s)
+char *string_toupper(char *s)
 {
 	int j = 0;
 
-	if (s[0] >= 'a' && s[0] <= 'z')
-	{
-		s[0] = s[0] - ' ';
-	}
-	j++;
 	while (s[j] != '\0')
 	{
-		if (s[j - 1] >= '\t' && s[j - 1] != '-' && s[j - 1] <= '.')
+		if (s[j] >= 'a' && s[j] <= 'z')
 		{
-			if (s[j] >= 'a' && s[j] <= 'z')
-			{
-				s[j] = s[j] - ' ';
-			}
+			s[j] = s[j] - ' ';
 		}
 		j++;
 	}
