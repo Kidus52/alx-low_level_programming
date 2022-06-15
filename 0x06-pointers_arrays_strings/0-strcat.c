803cfb593c3107e27b773a9f904f17791e629d1d
kidus52 <kidustigistu3410@gmail.com>
2022-06-15 10:30:47 -0700

    my commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,29 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
-* *_strcat - function commute srtings
-* @dest: param pointer to a char
-* @src: param pointer to a char
-* Return: return value of dest
+* _strcat - concatenates two strings
+* @dest: string 1
+* @src: string 2
+* Return: concatenated string
 */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
+	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
-
 	dest[i] = '\0';
+
 	return (dest);
 }
